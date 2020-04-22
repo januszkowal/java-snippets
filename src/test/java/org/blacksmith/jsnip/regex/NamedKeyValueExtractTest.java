732b864929e2e1ad0f7c7b67cbb62fa5c6c5db11
Janusz Kowal <jkowalrz@gmail.com>
2020-04-22 18:38:36 +0200

    Regex

--- src/test/java/org/blacksmith/jsnip/regex/NamedKeyValueExtractTest.java
+++ src/test/java/org/blacksmith/jsnip/regex/NamedKeyValueExtractTest.java
@@ -11,12 +11,39 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class NamedKeyValueExtractTest {
-  private static final Pattern pattern =
+  private static final Pattern patternWithWhiteSpaceCharacter =
       compile("(?<key>\\w+)\\s+(?<value>\\w+)(?=&|$)");
+  private static final Pattern patternWithWhiteEqCharacter1 =
+      compile("(?<key>\\w+)=(?<value>\\w+)(?=&|$)");
+  private static final Pattern patternWithWhiteEqCharacter2 =
+      compile("(?<key>\\w+)\\=(?<value>\\w+)(?=&|$)");
+  @Test
+  public void extractKeyValueSpacesTest() {
+    Matcher m = patternWithWhiteSpaceCharacter.matcher("type1 value1&type2 value2");
+    List<Pair<String,String>> pairs = new ArrayList<>();
+    while (m.find()) {
+      pairs.add(Pair.of(m.group("key"),m.group("value")));
+    }
+    Assertions.assertIterableEquals(List.of(
+        Pair.of("type1","value1"),
+        Pair.of("type2","value2")),pairs);
+  }
 
   @Test
-  public void extractKeyValueTest() {
-    Matcher m = pattern.matcher("type1 value1&type2 value2");
+  public void extractKeyValueEq1Test() {
+    Matcher m = patternWithWhiteEqCharacter1.matcher("type1=value1&type2=value2");
+    List<Pair<String,String>> pairs = new ArrayList<>();
+    while (m.find()) {
+      pairs.add(Pair.of(m.group("key"),m.group("value")));
+    }
+    Assertions.assertIterableEquals(List.of(
+        Pair.of("type1","value1"),
+        Pair.of("type2","value2")),pairs);
+  }
+
+  @Test
+  public void extractKeyValueEq2Test() {
+    Matcher m = patternWithWhiteEqCharacter2.matcher("type1=value1&type2=value2");
     List<Pair<String,String>> pairs = new ArrayList<>();
     while (m.find()) {
       pairs.add(Pair.of(m.group("key"),m.group("value")));
