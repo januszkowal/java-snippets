deac13c83c3136647704bbb47decbd12a851a9bc
januszkowal <jkowalrz@gmail.com>
2020-10-06 09:47:13 +0200

    Datasource config

---
+++ src/main/java/org/blacksmith/jsnip/config/datasource/DataSourceConfig1.java
@@ -0,0 +1,62 @@
+package org.blacksmith.jsnip.config.datasource;
+
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@EnableJpaRepositories(
+    basePackages = "com.model.repo",
+    entityManagerFactoryRef = "db1EntityManagerFactory",
+    transactionManagerRef = "db1TransactionManager")
+public class DataSourceConfig1 {
+
+  @Primary
+  @Bean(name = "db1JpaProperties")
+  @ConfigurationProperties(prefix = "db1.datasource.jpa")
+  public JpaProperties db1JpaProperties() {
+    return new JpaProperties();
+  }
+
+  @Primary
+  @Bean(name = "db1DataSource")
+  @ConfigurationProperties("db1.datasource")
+  public DataSource db1DataSource() {
+    return DataSourceBuilder.create().build();
+//    return db1DataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+  }
+
+  @Primary
+  @PersistenceContext(unitName = "db1")
+  @Bean(name = "db1EntityManagerFactory")
+  public LocalContainerEntityManagerFactoryBean db1EntityManagerFactory(EntityManagerFactoryBuilder builder,
+      @Qualifier("db1DataSource") DataSource dataSource,
+      @Qualifier("db1JpaProperties") JpaProperties jpaProperties) {
+    return builder.dataSource(dataSource)
+        .persistenceUnit("db1PersistenceUnit")
+        .packages("com.model.db1.entity")
+        .properties(jpaProperties.getProperties())
+        .build();
+  }
+
+  @Primary
+  @Bean(name = "db1TransactionManager")
+  public PlatformTransactionManager db1TransactionManager(
+      @Qualifier("db1DataSource") DataSource dataSource,
+      @Qualifier("db1EntityManagerFactory") LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+    return new JpaTransactionManager(entityManagerFactory.getObject());
+  }
+
+}
